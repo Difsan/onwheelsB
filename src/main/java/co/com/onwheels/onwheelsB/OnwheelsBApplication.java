da683bfacdb5da84f6b53fa291bf0d820da475b3
Difsan <difsanchezru@gmail.com>
2023-05-10 13:51:59 -0500

    Initial commit

---
+++ src/main/java/co/com/onwheels/onwheelsB/OnwheelsBApplication.java
@@ -0,0 +1,13 @@
+package co.com.onwheels.onwheelsB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnwheelsBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnwheelsBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/onwheels/onwheelsB/OnwheelsBApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.onwheels.onwheelsB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnwheelsBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
